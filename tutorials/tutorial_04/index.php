64d2a7665c266f92737989a799dd92ab49c1fc66
HeinHtetSan <htikehtet566@gmial.com>
2021-12-16 13:42:18 +0630

    [master] finished php tutorial 4

---
+++ tutorials/tutorial_04/index.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html>
+
+<head>
+  <title></title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div id="login">
+    <h1>You need to login</h1>
+    <form action="login.php" method="post">
+      <label for="id">User ID</label>
+      <input type="text" name="id" id="id">
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password">
+      <input type="submit" value="Login">
+    </form>
+
+  </div>
+</body>
+
+</html>
