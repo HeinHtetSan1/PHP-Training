d7bc70799733de2f61220cb0e3967875399196dd
HeinHtetSan <htikehtet566@gmial.com>
2021-12-17 22:50:28 +0630

    [master] finished update php tutorial 2

--- tutorials/tutorial_02/index.php
+++ tutorials/tutorial_02/index.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <title>Tutorial_02</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Tutorial_02</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
+
 <body>
-  <?php
-  $total = 6;
-  for ($r = 1; $r <= $total; $r++) {
-    for ($space = $total - $r; $space >= 1; $space--) {
-      echo " 	&nbsp;";
-    }
-    for ($star = 1; $star <= 2 * $r - 1; $star++) {
-      echo ("*");
-    }
-    echo "<br>";
-  }
-  for ($r = $total - 1; $r >= 1; $r--) {
-    for ($space = 1; $space <=  $total - $r; $space++) {
-      echo " 	&nbsp;";
-    }
-    for ($star = 1; $star <= 2 * $r - 1; $star++) {
-      echo ("*");
-    }
-    echo "<br>";
-  }
-  ?>
+	<?php
+	$total = 6;
+	for ($r = 1; $r <= $total; $r++) {
+		for ($space = $total - $r; $space >= 1; $space--) {
+			echo " 	&nbsp;";
+		}
+		for ($star = 1; $star <= 2 * $r - 1; $star++) {
+			echo ("*");
+		}
+		echo "<br>";
+	}
+	for ($r = $total - 1; $r >= 1; $r--) {
+		for ($space = 1; $space <=  $total - $r; $space++) {
+			echo " 	&nbsp;";
+		}
+		for ($star = 1; $star <= 2 * $r - 1; $star++) {
+			echo ("*");
+		}
+		echo "<br>";
+	}
+	?>
 </body>
+
 </html>
