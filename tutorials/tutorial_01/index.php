0c2fa6350e0c3b6635a6f6a54790c4402380f084
HeinHtetSan <htikehtet566@gmial.com>
2021-12-17 22:51:21 +0630

    [master] finished update php tutorial 1

--- tutorials/tutorial_01/index.php
+++ tutorials/tutorial_01/index.php
@@ -1,27 +1,29 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <title>Tutorial_01</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Tutorial_01</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
+
 <body>
-  <table cellspacing="0px" cellpadding="0px" border="1px">
-    <?php
-    //for loop
-    for ($row = 1; $row <= 8; $row++) {
-      echo "<tr>";
-      for ($col = 1; $col <= 8; $col++) {
-        $total = $row + $col;
-        if ($total % 2 == 0) {
-          echo "<td height=40px width=40px bgcolor=#FFFFFF></td>";
-        } else {
-          echo "<td height=40px width=40px bgcolor=#000000></td>";
-        }
-      }
-      echo "</tr>";
-    }
-    ?>
-  </table>
+	<table cellspacing="0px" cellpadding="0px" border="1px">
+		<?php
+		//for loop
+		for ($row = 1; $row <= 8; $row++) {
+			echo "<tr>";
+			for ($col = 1; $col <= 8; $col++) {
+				$total = $row + $col;
+				if ($total % 2 == 0) {
+					echo "<td height=40px width=40px bgcolor=#FFFFFF></td>";
+				} else {
+					echo "<td height=40px width=40px bgcolor=#000000></td>";
+				}
+			}
+			echo "</tr>";
+		}
+		?>
+	</table>
 </body>
 
 </html>
