24b926f52bc5013fe8244e88168550fd56216884
HeinHtetSan <htikehtet566@gmial.com>
2021-12-21 15:34:51 +0630

    [master] finished php tutorial 7

---
+++ tutorials/tutorial_07/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+
+<body>
+	<form action="qr.php" method="post">
+		<label for="">Enter QR text</label>
+		<input type="text" name="text"><br>
+		<input type="submit" name="submit" value="submit">
+	</form>
+</body>
+
+</html>
